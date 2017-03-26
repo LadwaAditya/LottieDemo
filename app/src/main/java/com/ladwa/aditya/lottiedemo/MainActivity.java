a95076f80e7cb0f60f51d462767dbb6a5689ad5f
Aditya Ladwa <ladwa.aditya@gmail.com>
2017-03-26 16:23:29 +0530

    chor(build.gradle): Added Basic layout and lottie lib

--- app/src/main/java/com/ladwa/aditya/lottiedemo/MainActivity.java
+++ app/src/main/java/com/ladwa/aditya/lottiedemo/MainActivity.java
@@ -1,13 +1,35 @@
 package com.ladwa.aditya.lottiedemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.ladwa.aditya.lottiedemo.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding mBinding;
+    private final String TOUCH_ID = "touch_id.json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button1:
+                mBinding.animationView.setAnimation(TOUCH_ID);
+                mBinding.animationView.playAnimation();
+                break;
+            case R.id.button2:
+                break;
+            case R.id.button3:
+                break;
+            case R.id.button4:
+                break;
+        }
     }
 }
